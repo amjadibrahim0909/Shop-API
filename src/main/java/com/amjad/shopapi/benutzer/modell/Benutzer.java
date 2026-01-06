4a855618f5ed3b00cf978c6a3cb9ea20967ae930
Amjad <amjad.ibrahim0909@gmail.com>
2026-01-06 14:22:28 +0100

    refactor: replace boilerplate getters and setters with Lombok in user entity

--- src/main/java/com/amjad/shopapi/benutzer/modell/Benutzer.java
+++ src/main/java/com/amjad/shopapi/benutzer/modell/Benutzer.java
@@ -1,10 +1,15 @@
 package com.amjad.shopapi.benutzer.modell;
 
-
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Benutzer {
 
     @Id
@@ -15,46 +20,4 @@ public class Benutzer {
     private String email;
     private String password;
     private String role;
-
-    // Getter und Setter
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
